3515bb8d72aa22be9b0a7ca01348af9f4e7b60a5
Pablo <pablo.esteban.sanchez@gmail.com>
2026-01-19 10:54:20 +0100

    Añadidas reglas al cortafuegos

--- gw/firewall/firewall.sh
+++ gw/firewall/firewall.sh
@@ -8,6 +8,7 @@ iptables -F
 iptables -t nat -F
 iptables -Z
 iptables -t nat -Z
+
 # ANTI-LOCK rule: Permitir ssh a través de ETH0 para acceder con vagrant
 iptables -A INPUT -i eth0 -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -j ACCEPT
@@ -20,7 +21,23 @@ iptables -P FORWARD DROP
 ############################
 # Reglas de protección local
 ############################
-# 1. Permitir tráfico de loopback
+# L1. Permitir tráfico de loopback
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+
+# L2. Permitir ping a cualquier máquina interna o externa
+iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+
+# L3. Permitir que mem hagan ping desde LAN y DMZ
+iptables -A INPUT -i eth2 -s 172.1.99.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -i eth3 -s 172.2.99.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A OUTPUT -o eth2 -s 172.1.99.1 -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT -o eth3 -s 172.2.99.1 -p icmp --icmp-type echo-reply -j ACCEPT
+
+# L4. Permitir consultas DNS
+iptables -A OUTPUT -o eth0 -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -i eth0 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 
 
